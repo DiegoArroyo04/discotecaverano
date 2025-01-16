1781364cb1f78708f34d4cfb8c593b784d6857c7
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-01-16 03:09:59 +0100

    empezando modal pop up ultimo evento

--- discotecaverano/src/componentes/modalHome/modalHome.tsx
+++ discotecaverano/src/componentes/modalHome/modalHome.tsx
@@ -1,36 +1,9 @@
-import React, { useState } from 'react';
-import './modalHome.css'; // Archivo CSS para estilos
+import './modalHome.css'
 
-// Definimos las propiedades del próximo evento usando una interfaz
-interface EventProps {
-  imageUrl: string;
-  title: string;
-  description: string;
-  date: string;
-}
+export default function modalHome() {
+  return (
+    <div>
 
-const modalHome: React.FC<EventProps> = ({ imageUrl, title, description, date }) => {
-  // Estado para manejar la visibilidad de la ventana emergente
-  const [isVisible, setIsVisible] = useState(true);
-
-  // Función para cerrar la ventana emergente
-  const handleClose = () => {
-    setIsVisible(false);
-  };
-
-  // Retorna la ventana emergente solo si es visible
-  return isVisible ? (
-    <div className="popup-overlay">
-      <div className="popup-content">
-        <button className="close-button" onClick={handleClose}>X</button>
-        <img src={imageUrl} alt={`Cartel de ${title}`} className="popup-image" />
-        <h2 className="popup-title">{title}</h2>
-        <p className="popup-description">{description}</p>
-        <p className="popup-date">Fecha: {date}</p>
-      </div>
     </div>
-  ) : null;
-};
-
-export default modalHome;
-
+  )
+}
