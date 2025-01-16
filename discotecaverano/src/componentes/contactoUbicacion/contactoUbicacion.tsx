05efef5034e57a05658ca22056bd476109329e19
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-01-16 01:15:40 +0100

    despliegue vercel

--- discotecaverano/src/componentes/contactoUbicacion/contactoUbicacion.tsx
+++ discotecaverano/src/componentes/contactoUbicacion/contactoUbicacion.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './contactoUbicacion.css'
 
 export default function contactoUbicacion() {
--- discotecaverano/src/componentes/eventos/eventos.tsx
+++ discotecaverano/src/componentes/eventos/eventos.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './eventos.css'
 
 type Evento = {
