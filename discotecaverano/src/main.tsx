196a23cf6b73e05b9c4a39958a6922ed307721e2
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-01-20 13:53:14 +0100

    preloader corto

--- discotecaverano/src/main.tsx
+++ discotecaverano/src/main.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
     // Simulate loading time of at least 2 seconds
     const timeout = setTimeout(() => {
       setIsLoading(false);
-    }, 9000);
+    }, 2000);
 
     return () => clearTimeout(timeout);
   }, []);
