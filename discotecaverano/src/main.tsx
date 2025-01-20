21e787037c634a5cd5027328a3e758a22e394bde
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-01-20 12:55:02 +0100

    preloader feo

--- discotecaverano/src/main.tsx
+++ discotecaverano/src/main.tsx
@@ -1,7 +1,6 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import { useEffect } from "react";
 import './index.css'
 import Header from './componentes/header/header.tsx'
 import Footer from './componentes/footer/footer.tsx'
@@ -13,6 +12,19 @@ import Eventos from './componentes/eventos/eventos.tsx'
 import UltimoEvento from './componentes/modalHome/modalHome.tsx'
 import Patrocinadores from './componentes/patrocinadores/patrocinadores.tsx';
 
+const Preloader: React.FC = () => {
+  return (
+    <div className="preloader">
+      <div className="logo-container">
+        <img src="/logos/logo.png" alt="Logo" className='logo_preloader' />
+        <div className='shadow'></div>
+      </div>
+    </div>
+  );
+
+}
+
+
 const Home: React.FC = () => {
 
   useEffect(() => {
@@ -21,6 +33,7 @@ const Home: React.FC = () => {
   }, []); // El array vacío significa que solo se ejecuta una vez al montarse el componente
 
   return (
+
     <>
       {/* Aquí va tu contenido */}
       <Header />
@@ -30,28 +43,56 @@ const Home: React.FC = () => {
       <ContactoUbicacion />
       <Footer />
     </>
+
   );
+
 }
 
 export default Home;
 
+const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading time of at least 2 seconds
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+
+    <Router>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+
+        <Routes>
+          {/*HOME*/}
+          <Route path="/" element={<Home />} />
+
+          {/*AVISO LEGAL*/}
+          <Route path="/avisoLegal" element={<AvisoLegal />} />
+
+          {/*POLITICA PRIVACIDAD*/}
+          <Route path="/politicaPrivacidad" element={<PoliticaPrivacidad />} />
+
+          {/*POLITICA COOKIES*/}
+          <Route path="/politicaCookies" element={<PoliticaCookies />} />
+
+        </Routes>
+      )}
+    </Router>
+
+  )
+};
+
+// Renderizar la aplicación
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        {/*HOME*/}
-        <Route path="/" element={<Home />} />
+    <App />
+  </StrictMode>
+);
 
-        {/*AVISO LEGAL*/}
-        <Route path="/avisoLegal" element={<AvisoLegal />} />
-
-        {/*POLITICA PRIVACIDAD*/}
-        <Route path="/politicaPrivacidad" element={<PoliticaPrivacidad />} />
-
-        {/*POLITICA COOKIES*/}
-        <Route path="/politicaCookies" element={<PoliticaCookies />} />
-
-      </Routes>
-    </Router>
-  </StrictMode>,
-)
