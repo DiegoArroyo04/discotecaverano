db4d3fab64d6120436f8672e626547ef28f21e24
DiegoArroyo04 <diegoarroyogonzalez04@gmail.com>
2025-02-02 19:21:41 +0100

    componente galerias terminado

--- discotecaverano/src/componentes/galeriaImagenes/galeria2023.tsx
+++ discotecaverano/src/componentes/galeriaImagenes/galeria2023.tsx
@@ -1,105 +1,75 @@
+import { useState } from 'react';
 import './galeriaImagenesAnios.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
-import { Element } from 'react-scroll';
-import { scroller } from 'react-scroll';
-import { useEffect } from 'react';
-import { FiArrowLeft } from "react-icons/fi"; // Icono de "volver atrás"
+import { FiArrowLeft, FiX, FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 
-export default function Galeria2023() {
+const imagenes = [
+  '/imagenes/2023Imagen1.png', '/imagenes/2023Imagen2.png', '/imagenes/2023Imagen3.png',
+  '/imagenes/2023Imagen4.png', '/imagenes/2023Imagen5.png', '/imagenes/2023Imagen6.png',
+  '/imagenes/2023Imagen7.png', '/imagenes/2023Imagen8.png', '/imagenes/2023Imagen9.png',
+  '/imagenes/2023Imagen10.png', '/imagenes/2023Imagen11.png', '/imagenes/2023Imagen12.png',
+  '/imagenes/2023Imagen13.png', '/imagenes/2023Imagen14.png', '/imagenes/2023Imagen15.png',
+  '/imagenes/2023Imagen16.png', '/imagenes/2023Imagen17.png', '/imagenes/2023Imagen18.png'
+];
 
-  const handleGoBack = () => {
-    window.history.back(); // Regresa a la página anterior
+export default function Galeria2023() {
+  const [imagenActual, setImagenActual] = useState<number | null>(null);
+
+  const abrirImagen = (index: number) => {
+    setImagenActual(index);
   };
 
-  useEffect(() => {
-    const hash = window.location.hash.slice(1); // Remueve el "#" del fragmento
-    if (hash) {
-      scroller.scrollTo(hash, {
-        duration: 800,
-        offset: -130,
-        smooth: 'easeInOutQuad',
-      });
-    }
+  const cerrarImagen = () => {
+    setImagenActual(null);
+  };
 
-  }, []);
+  const siguienteImagen = () => {
+    if (imagenActual !== null) {
+      setImagenActual((imagenActual + 1) % imagenes.length);
+    }
+  };
+
+  const anteriorImagen = () => {
+    if (imagenActual !== null) {
+      setImagenActual((imagenActual - 1 + imagenes.length) % imagenes.length);
+    }
+  };
 
   return (
     <div>
       <Header />
-      <Element name="galeria2023">
-        <h1 className='tituloGaleria'>Galeria 2023</h1>
-        {/* Galería */}
-        <div className="galeria">
-          <div className="galeria-grid">
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen1.png' className="galeria-img"></img>
+      <h1 className='tituloGaleria'>Galería 2023</h1>
+      <div className="galeria">
+        <div className="galeria-grid">
+          {imagenes.map((img, index) => (
+            <div key={index} className="galeria-item" onClick={() => abrirImagen(index)}>
+              <img src={img} className="galeria-img" alt={`Imagen ${index + 1}`} />
             </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen2.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen3.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen4.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen5.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen6.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen7.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen8.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen9.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen10.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen11.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen12.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen13.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen14.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen15.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen16.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen17.png' className="galeria-img"></img>
-            </div>
-            <div className="galeria-item">
-              <img src='/imagenes/2023Imagen18.png' className="galeria-img"></img>
-            </div>
-          </div>
+          ))}
         </div>
-      </Element>
+      </div>
       <Footer />
 
       {/* Botón para volver atrás */}
       <Link to="/#galeriaImagenes">
-        <button className="go-back-btn" onClick={handleGoBack}>
-          <FiArrowLeft size={20} style={{ marginRight: "8px" }} />
-          Volver
+        <button className="go-back-btn">
+          <FiArrowLeft size={20} style={{ marginRight: "8px" }} /> Volver
         </button>
       </Link>
 
+      {/* Visor de imágenes en pantalla completa */}
+      {imagenActual !== null && (
+        <div className="modal" onClick={cerrarImagen}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <button className="cerrarImagen" onClick={cerrarImagen}><FiX size={30} /></button>
+            <button className="anterior" onClick={anteriorImagen}><FiChevronLeft size={50} /></button>
+            <img src={imagenes[imagenActual]} className="imagen-grande" alt="Imagen ampliada" />
+            <button className="siguiente" onClick={siguienteImagen}><FiChevronRight size={50} /></button>
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
--- discotecaverano/src/componentes/galeriaImagenes/galeria2024.tsx
+++ discotecaverano/src/componentes/galeriaImagenes/galeria2024.tsx
@@ -1,102 +1,75 @@
+import { useState } from 'react';
 import './galeriaImagenesAnios.css';
 import Header from '../header/header';
 import Footer from '../footer/footer';
-import { scroller } from 'react-scroll';
-import { useEffect } from 'react';
-import { Element } from 'react-scroll';
-import { FiArrowLeft } from "react-icons/fi"; // Icono de "volver atrás"
+import { FiArrowLeft, FiX, FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { Link } from "react-router-dom";
 
+const imagenes = [
+    '/imagenes/2024Imagen1.png', '/imagenes/2024Imagen2.png', '/imagenes/2024Imagen3.png',
+    '/imagenes/2024Imagen4.png', '/imagenes/2024Imagen5.png', '/imagenes/2024Imagen6.png',
+    '/imagenes/2024Imagen7.png', '/imagenes/2024Imagen8.png', '/imagenes/2024Imagen9.png',
+    '/imagenes/2024Imagen10.png', '/imagenes/2024Imagen11.png', '/imagenes/2024Imagen12.png',
+    '/imagenes/2024Imagen13.png', '/imagenes/2024Imagen14.png', '/imagenes/2024Imagen15.png',
+    '/imagenes/2024Imagen16.png', '/imagenes/2024Imagen17.png', '/imagenes/2024Imagen18.png'
+];
+
 export default function Galeria2024() {
-    const handleGoBack = () => {
-        window.history.back(); // Regresa a la página anterior
+    const [imagenActual, setImagenActual] = useState<number | null>(null);
+
+    const abrirImagen = (index: number) => {
+        setImagenActual(index);
     };
 
-    useEffect(() => {
-        const hash = window.location.hash.slice(1); // Remueve el "#" del fragmento
-        if (hash) {
-            scroller.scrollTo(hash, {
-                duration: 800,
-                offset: -130,
-                smooth: 'easeInOutQuad',
-            });
-        }
+    const cerrarImagen = () => {
+        setImagenActual(null);
+    };
 
-    }, []);
+    const siguienteImagen = () => {
+        if (imagenActual !== null) {
+            setImagenActual((imagenActual + 1) % imagenes.length);
+        }
+    };
+
+    const anteriorImagen = () => {
+        if (imagenActual !== null) {
+            setImagenActual((imagenActual - 1 + imagenes.length) % imagenes.length);
+        }
+    };
 
     return (
         <div>
             <Header />
-            <Element name="galeria2024">
-                <h1 className='tituloGaleria'>Galeria 2024</h1>
-                {/* Galería */}
-                <div className="galeria">
-                    <div className="galeria-grid">
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen1.png' className="galeria-img"></img>
+            <h1 className='tituloGaleria'>Galería 2024</h1>
+            <div className="galeria">
+                <div className="galeria-grid">
+                    {imagenes.map((img, index) => (
+                        <div key={index} className="galeria-item" onClick={() => abrirImagen(index)}>
+                            <img src={img} className="galeria-img" alt={`Imagen ${index + 1}`} />
                         </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen2.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen3.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen4.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen5.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen6.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen7.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen8.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen9.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen10.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen11.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen12.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen13.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen14.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen15.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen16.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen17.png' className="galeria-img"></img>
-                        </div>
-                        <div className="galeria-item">
-                            <img src='/imagenes/2024Imagen18.png' className="galeria-img"></img>
-                        </div>
-                    </div>
+                    ))}
                 </div>
-            </Element>
+            </div>
             <Footer />
+
             {/* Botón para volver atrás */}
             <Link to="/#galeriaImagenes">
-                <button className="go-back-btn" onClick={handleGoBack}>
-                    <FiArrowLeft size={20} style={{ marginRight: "8px" }} />
-                    Volver
+                <button className="go-back-btn">
+                    <FiArrowLeft size={20} style={{ marginRight: "8px" }} /> Volver
                 </button>
             </Link>
+
+            {/* Visor de imágenes en pantalla completa */}
+            {imagenActual !== null && (
+                <div className="modal" onClick={cerrarImagen}>
+                    <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                        <button className="cerrarImagen" onClick={cerrarImagen}><FiX size={30} /></button>
+                        <button className="anterior" onClick={anteriorImagen}><FiChevronLeft size={50} /></button>
+                        <img src={imagenes[imagenActual]} className="imagen-grande" alt="Imagen ampliada" />
+                        <button className="siguiente" onClick={siguienteImagen}><FiChevronRight size={50} /></button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
+    );
 }
